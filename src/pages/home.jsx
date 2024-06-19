f5bda2d28884c8a61fcacdace2261fcf7c513092
Cristian-Miguel <cristian_enano_99@hotmail.com>
2024-06-18 20:41:59 -0600

    Card of product

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,44 +1,110 @@
 import React from 'react';
-import { Col, Row, Layout, Flex} from "antd";
+import { Col, Row, Layout, Card, Flex, message } from "antd";
 import PromosImage from '../assets/daniela-cuevas-t7YycgAoVSw-unsplash.jpg';
+import CardImage from '../assets/pexels-eberhardgross-1062249.jpg';
 import MainHeader from '../components/layout/header';
 import MainFooter from '../components/layout/footer';
 import '../css/home.css';
 
 const Home = () => {
+
+    const handlerClick = () => {
+        message.info("click on me");
+    }
+
     return(
         <>
             <div id='HomeComponent'>
                 <Layout>
                     <MainHeader/>
                     
-                    <Layout.Content style={{ minHeight: 1500, width:'100%' }}>
-                        <Flex justify='center'>
-                            <h1>Home</h1>
-                            <p>Discover your best adventure</p>
-                        </Flex>
-
-                        <Row justify={'center'} align={'middle'}>
+                    <Layout.Content style={{ minHeight: 1000, width:'100%' }}>
+                        
+                        <Row justify={'center'} align={'middle'} style={{ marginTop:40 }}>
                             <Col span={2}></Col>
                             <Col span={20}>
                                 <figure id = 'PromosImageContainer'>
-                                    <img src={PromosImage} alt="promotion image" id='PromosContainer' />
+                                    <div class="overlay"></div>
+                                    <h1 style={{ textAlign:'center' }} >Discover Mich</h1>
+                                    <p style={{ textAlign:'center' }} >Discover your best adventure</p>
+                                    <img src={PromosImage} alt="Main title" id='PromosContainer' />
                                 </figure>
                             </Col>
                             <Col span={2}></Col>
                         </Row>
 
-                        <Row>
-                            <Col span={2}>2</Col>
-                            <Col span={10}>10</Col>
-                            <Col span={10}>10</Col>
-                            <Col span={2}>2</Col>
+                        <Row justify={'center'} style={{ marginTop:20 }}>
+                            <Col span={2}></Col>
+                            <Col span={20}>
+                                <h3 style={{ textAlign:'center' }} >Better range</h3>
+                            </Col>
+                            <Col span={2}></Col>
                         </Row>
 
-                        <Row>
-                            <Col span={2}>2</Col>
-                            <Col span={20}>20</Col>
-                            <Col span={2}>2</Col>
+                        <Row justify={'center'} style={{ marginTop:20 }}>
+                            <Col span={2}></Col>
+
+                            <Col span={5}>
+                                <Flex justify='center'>
+                                    <Card
+                                        hoverable
+                                        style={{
+                                        width: 240,
+                                        }}
+                                        cover={<img alt="example" src={CardImage} />}
+                                        onClick={handlerClick}
+                                    >
+                                        <p>card 1</p>
+                                    </Card>
+                                </Flex>
+                            </Col>
+
+                            <Col span={5}>
+                                <Flex justify='center'>
+                                    <Card
+                                        hoverable
+                                        style={{
+                                        width: 240,
+                                        }}
+                                        cover={<img alt="example" src={CardImage} />}
+                                        onClick={handlerClick}
+                                    >
+                                        <p>card 1</p>
+                                    </Card>
+                                </Flex>
+                            </Col>
+
+                            <Col span={5}>
+                                <Flex justify='center'>
+                                    <Card
+                                        hoverable
+                                        style={{
+                                        width: 240,
+                                        }}
+                                        cover={<img alt="example" src={CardImage} />}
+                                        onClick={handlerClick}
+                                    >
+                                        <p>card 1</p>
+                                    </Card>
+                                </Flex>
+                            </Col>
+
+                            <Col span={5}>
+                                <Flex justify='center'>
+                                    <Card
+                                        hoverable
+                                        style={{
+                                            width: 240,
+                                        }}
+                                        cover={<img alt="example" src={CardImage} />}
+                                        onClick={handlerClick}
+                                    >
+                                        <p>card 1</p>
+                                    </Card>
+                                </Flex>
+                            </Col>
+
+                            <Col span={2}></Col>
                         </Row>
                     </Layout.Content>
 
